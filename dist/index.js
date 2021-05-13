db2cc17aca7c3fb51de45c3e2c3e355486004033
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-05-13 23:36:20 +0530

    added prettier and started to actually code

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,23 @@
 #!/usr/bin/env node
+const berryArgs = process.argv.slice(2);
+const cwd = process.cwd();
+const commandType = berryArgs[0];
+switch (commandType) {
+    case 'add':
+        if (berryArgs[1] === 'editorconfig') {
+            addEditorConfig();
+        }
+        else {
+            console.log('what do i add');
+        }
+        break;
+    case 'pwd':
+        console.log(process.cwd());
+        break;
+    default:
+        console.log('default what!');
+}
+function addEditorConfig() {
+    console.log('Added an editor config');
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,27 @@
  * 2) Kill a process on a particular port.
  * 3) ...
  */
+
+const berryArgs = process.argv.slice(2);
+const cwd = process.cwd();
+
+const commandType = berryArgs[0];
+
+switch (commandType) {
+    case 'add':
+        if (berryArgs[1] === 'editorconfig') {
+            addEditorConfig();
+        } else {
+            console.log('what do i add');
+        }
+        break;
+    case 'pwd':
+        console.log(process.cwd());
+        break;
+    default:
+        console.log('default what!');
+}
+
+function addEditorConfig() {
+    console.log('Added an editor config');
+}
