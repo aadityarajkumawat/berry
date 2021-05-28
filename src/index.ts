f24df7472bade99e785899fc77fa0acd8b60f96b
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-05-28 23:26:46 +0530

    ready for production

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+import chalk from 'chalk';
 import { State } from './elephant/manageState';
 import { addEditorConfig } from './olives/addEditorConfig';
 import { addGitIgnore } from './olives/addGitIgnore';
@@ -17,6 +18,7 @@ import { printCWD } from './olives/printCWD';
 
 const berryArgs = process.argv.slice(2);
 const commandType = berryArgs[0];
+const $3s = '   ';
 
 switch (commandType) {
     case 'add':
@@ -44,6 +46,31 @@ switch (commandType) {
     case 'pwd':
         printCWD();
         break;
+    case '--help':
+    case '-h':
+        console.log(`   Usage
+   [commands]:
+        add: adds the required file.
+        kill-port: kills the running process on a port
+        pwd: gives the current path
+
+   [add command]:
+        editorconfig - adds an editorconfig file
+        gitignore - adds a gitignore file
+
+   [kill-port]:
+        <port-number> - kills process on a particular port
+        `);
+        break;
     default:
-        console.log('default what!');
+        console.log(chalk.greenBright(`${$3s}|||| Welcome to berryx ||||`));
+        console.log(
+            `\n${$3s}Berryx was developed to do tasks that I don't like\n${$3s}doing manaually or are better to do using terminal`,
+        );
+        console.log(
+            `\n${$3s}Github Repository: https://github.com/aadityarajkumawat/berry\n${$3s}--help | -h: To see all options`,
+        );
+        console.log(
+            chalk.hex('#dccfff').bold(`\n${$3s}Author: Aditya Raj Kumawat`),
+        );
 }
